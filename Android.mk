ff019ba34cdbce9637ffa60690df42b42adc0736
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-11-21 14:53:49 +0800

    use LOCAL_REQUIRED_MODULES to build with android tree

--- Android.mk
+++ Android.mk
@@ -30,7 +30,9 @@ LOCAL_PACKAGE_NAME := AndroidTerm
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_JNI_SHARED_LIBRARIES := libjackpal-androidterm2
+LOCAL_REQUIRED_MODULES := libjackpal-androidterm2
+
+LOCAL_OVERRIDES_PACKAGES := ConnectBot
 
 include $(BUILD_PACKAGE)
 
